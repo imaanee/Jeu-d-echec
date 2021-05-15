04f0959dc547000f4be11ec39da630a0838fe0db
Imane Mihoubi <mihoubi.imane1@gmail.com>
2021-05-15 23:33:46 +0200

    class Piece

---
+++ Piece.java
@@ -0,0 +1,27 @@
+public class Piece {
+
+	// Piece définiti par un type et une couleur
+
+	private String type;
+	private String couleur;
+	
+	
+	public Piece(String unType, String uneCouleur) {
+		this.type    = unType;
+		this.couleur = uneCouleur;
+	}
+	
+	public String getType () {
+		return this.type;
+	}
+	
+	public String getCouleur () {
+		return this.couleur;
+	}
+
+	public String toString () {
+		return this.getType() + this.getCouleur();
+	}
+
+
+}
