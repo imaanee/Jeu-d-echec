a52ff195d35b634cd74b32547208a8de8a7c8d03
Imane Mihoubi <mihoubi.imane1@gmail.com>
2021-05-21 14:01:13 +0200

    ajout abstract + attributs de position x et y

--- Piece.java
+++ Piece.java
@@ -1,25 +1,34 @@
-public class Piece {
+public abstract class Piece {
 
-	// Piece définiti par un type et une couleur
+	// Une piece est définie par un type et une couleur
 
 	private String type;
 	private String couleur;
+	private String posX;
+	private String posY;
 	
 	
-	public Piece(String unType, String uneCouleur) {
+	public Piece(String unType, String uneCouleur, String initX, String initY) 
+	{
 		this.type    = unType;
 		this.couleur = uneCouleur;
+		this.posX = initX;
+		this.posY = initY;
 	}
+	
 	
 	public String getType () {
 		return this.type;
 	}
 	
-	public String getCouleur () {
+	public String getCouleur () 
+	{
 		return this.couleur;
 	}
 
-	public String toString () {
+
+	public String toString () 
+	{
 		return this.getType() + this.getCouleur();
 	}
 
