d47319e384e971a8366357189eea7abeb4db18b3
souhaylaLT <84252144+souhaylaLT@users.noreply.github.com>
2021-06-04 20:33:16 +0200

    Create Tour.java

---
+++ Tour.java
@@ -0,0 +1,23 @@
+public class Tour extends Piece {
+	
+	
+	public Tour(String couleur , String type)
+	{
+		super(couleur , type);
+		
+		
+	}
+	public String toString()
+	{
+		s = "type=" + this.getType() + "/n" + "couleur=" + this.getCouleur();
+		return s;
+		
+		
+	}
+	
+	public boolean move( int Depx)
+	{
+		
+		
+		
+	}
