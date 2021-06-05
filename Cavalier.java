356be17196fb34b7a736b5118588fb945e511fe9
Imane Mihoubi <83464837+imaanee@users.noreply.github.com>
2021-06-05 16:25:29 +0200

    Create Cavalier.java
    premiere version classe Cavalier

---
+++ Cavalier.java
@@ -0,0 +1,56 @@
+public class Cavalier extends Piece {
+	
+	// CONSTRUCTEUR ===================================================================
+	
+	/**
+	 *   Initialise un cavalier 
+	 */	
+	public Cavalier(boolean uneCouleur, Case c) 
+	{
+		super(uneCouleur, c);
+	}
+	// fin du constructeur
+	
+	
+	// METHODES ========================================================================
+	
+	/**
+	 *   Verifie si le deplacement du depart a l'arrive est valide
+	 * return true si c'est le cas et false sinon
+	 */		
+	public boolean deplacementValid(Case arrivee)
+	{
+		Case depart = this.getCase();
+		int ligneDep = depart.getLigne()  ;
+		int colDep   = depart.getColonne();
+		
+		int ligneArr = arrivee.getLigne()  ;
+		int colArr   = arrivee.getColonne();
+		
+		if (depart == arrivee) return false;
+		
+		int verifL = (ligneArr - ligneDep) * (ligneArr - ligneDep);
+		int verifC = (colArr   - colDep)   * (colArr   - colDep)  ;
+		
+		if ( (verifL + verifC) == 5) return true;
+		
+		return false;		
+	}
+	// fin methode deplacement valide
+	
+
+	
+	// METHODES UTILITAIRES ============================================================
+	
+	/**
+	 *  retourne la chaine de caracteres representant le cavalier courant
+	 */
+	public String toString() 
+	{
+		return "je suis un cavalier " + super.toString();		
+	}
+	//fin methode toString
+	
+	
+}
+// fin classe Cavalier
