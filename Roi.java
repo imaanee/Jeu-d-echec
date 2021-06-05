4223373b284969c048f3d35cefdf792efae75d20
Imane Mihoubi <83464837+imaanee@users.noreply.github.com>
2021-06-05 16:42:00 +0200

    Create Roi.java
    Premiere version classe Roi

---
+++ Roi.java
@@ -0,0 +1,55 @@
+public class Roi extends Piece {
+	
+	// CONSTRUCTEUR ===================================================================
+	
+	/**
+	 *   Initialise un roi 
+	 */	
+	public Roi(boolean uneCouleur, Case c) 
+	{
+		super(uneCouleur, c);
+	}
+	// fin du constructeur
+	
+	
+	// METHODES ========================================================================
+	
+	/**
+	 *   Verifie si le deplacement du depart a l'arrive est valide
+	 * return true si c'est le cas et false sinon
+	 */		
+	public boolean deplacementValid(Case arrivee)
+	{
+		Case depart = this.getCase();
+		int ligneDep = depart.getLigne()  ;
+		int colDep   = depart.getColonne();
+		
+		int ligneArr = arrivee.getLigne()  ;
+		int colArr   = arrivee.getColonne();
+		
+		if (depart == arrivee) return false;
+		
+		if (ligneArr > ligneDep + 1 || ligneArr < ligneDep - 1) return false;
+		
+		if (colArr > colDep + 1 || colArr < colDep - 1) return false;
+		
+		return true;		
+	}
+	// fin methode deplacement valide
+	
+
+	
+	// METHODES UTILITAIRES ============================================================
+	
+	/**
+	 *  retourne la chaine de caracteres representant le roi courant
+	 */
+	public String toString() 
+	{
+		return "je suis un roi " + super.toString();		
+	}
+	//fin methode toString
+	
+	
+}
+// fin classe Roi
