5e6a022ab51f91646ed685ec07453539c7587a1c
Imane Mihoubi <83464837+imaanee@users.noreply.github.com>
2021-06-05 13:57:17 +0200

    Update Case.java
    Quelques erreurs (oublie parenthèse méthode toString, il faudrait retourner un nom de couleur et non un boolean, pareil pour la colonne garder le int mais retourner la lettre qui correspond)
    Ajout des méthodes getNomCouleur et getNomColonne

--- Case.java
+++ Case.java
@@ -1,44 +1,99 @@
-public class  Case 
-{
-	private int ligne ; 
-    private int colonne ;// je pense que c'est mieux de mettre int parce qu'avant d'etre un échiquier , c'est un tableau  (plateau ) avec 8 colonnes et 8 lignes ,  je propose qu'on fasse une méthode qui attribut les lettres des colonnes // 
-	private boolean couleur ;
-    
-    public Case( String uneLigne , String uneColonne , uneCouleur )
-	{
-		this.ligne = uneLigne ; 
-		this.colonne = uneColonne ;
-		this.couleur = uneCouleur ;
-	}
-    
-    public String getLigne()
-	{
-		return this.ligne ;
-		
-	}
+public class Case {
 	
-	public String getColonne()
+	// VARIABLES ======================================================================
+	
+	private boolean couleur;
+	private int     ligne  ;
+	private int     colonne;
+	
+	// CONSTRUCTEUR ===================================================================
+	
+	/**
+	 *   Initialise une case
+	 */
+	public Case(boolean uneCouleur, int uneLigne, int uneColonne)
 	{
-		
-		return this.getColonne;
+		this.couleur = uneCouleur;
+		this.ligne   = uneLigne  ;
+		this.colonne = uneColonne;
 	}
+	// fin du constructeur
 	
 	
+	// METHODES D'ACCES ===============================================================
+	
+	/**
+	 *   retourne le boolean de la case courante
+	 */
 	public boolean getCouleur()
 	{
-		return this.couleur ;
+		return this.couleur;
 	}
+	// fin methode getCouleur
+	
+	
+	/**
+	 *   retourne la couleur de la piece courante
+	    on admettra que true pour une piece blanche et false une piece noire
+	 */
+	public String getNomCouleur()
+	{
+		if (couleur)
+			return "blanche";
+		else
+			return "noire";
+	}
+	// fin methode getNomCouleur
+	
+	
+	/**
+	 *   retourne la ligne de la case courante
+	 */
+	public int getLigne()
+	{
+		return this.ligne;
+	}
+	// fin methode getLigne
+	
+	
+	/**
+	 *   retourne le chiffre correspondant à la colonne de la case courante
+	 */
+	public int getColonne()
+	{
+		return this.colonne;
+	}
+	// fin methode getColonne
+	
+	
+	/**
+	 *   retourne le nom de colonne de la case courante
+	 */
+	public String getNomColonne()
+	{
+		switch(this.colonne)
+		{
+			case 1  : return "A" ;
+			case 2  : return "B" ;
+			case 3  : return "C" ;
+			case 4  : return "D" ;
+			case 5  : return "E" ;
+			case 6  : return "F" ;
+			case 7  : return "G" ;
+			case 8  : return "H" ;
+			default : return "?" ;
+		}
+	}
+	// fin methode getNomColonne
 	
 	
 	public String toString()
 	{
-		
-		String str = "ligne = " + this.getLigne +"\n"
-		             "colonne = " + this.getColonne + "\n"
-					 "couleur = "  + this.getCouleur ;
-		return str;
+		return "je suis une piece : " + this.getNomCouleur() +
+			   " sur la ligne "       + this.getLigne()      +   
+			   " et sur la colonne "  + this.getNomColonne()    ;
 	}
 	
 
-
 }
+// fin de la classe Case
